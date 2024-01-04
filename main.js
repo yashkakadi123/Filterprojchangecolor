f2f52d844279bce6803895898c456800f696385a
yashkakadi123 <151625741+yashkakadi123@users.noreply.github.com>
2024-01-04 20:55:23 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+function preload()
+{
+
+}
+ 
+function setup()
+{
+canvas = createCanvas(300,300);
+canvas.center();
+}
+
+function draw()
+{
+}
+function take_snapshot()
+{
+ save('myFilterImage.png')
+}
